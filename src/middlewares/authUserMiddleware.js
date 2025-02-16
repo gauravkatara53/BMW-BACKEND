2022fceba70360351d94627ed262dc9eeaee782e
gauravkatara53 <gauravkatara53@gmail.com>
2025-02-16 23:08:58 +0530

    Af

--- src/middlewares/authUserMiddleware.js
+++ src/middlewares/authUserMiddleware.js
@@ -2,6 +2,9 @@ import { ApiError } from '../utils/ApiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/userModel.js';
+import NodeCache from 'node-cache';
+
+const cache = new NodeCache({ stdTTL: 600, checkperiod: 120 }); // Cache for 10 mins
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
@@ -9,19 +12,29 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header('Authorization')?.replace('Bearer ', '');
 
-    // console.log(token);
     if (!token) {
       throw new ApiError(401, 'Unauthorized request');
     }
 
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const cacheKey = `token_${token}`;
+    let decodedToken = cache.get(cacheKey);
 
-    const user = await User.findById(decodedToken?._id).select(
-      '-password -refreshToken'
-    );
+    if (!decodedToken) {
+      decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      cache.set(cacheKey, decodedToken);
+    }
+
+    const userCacheKey = `user_${decodedToken?._id}`;
+    let user = cache.get(userCacheKey);
 
     if (!user) {
-      throw new ApiError(401, 'Invalid Access Token');
+      user = await User.findById(decodedToken?._id).select(
+        '-password -refreshToken'
+      );
+      if (!user) {
+        throw new ApiError(401, 'Invalid Access Token');
+      }
+      cache.set(userCacheKey, user);
     }
 
     req.user = user;
